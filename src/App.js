881d6b03e552697b68d921cffc989d0750950e7a
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-08 15:02:27 +0530

    like count feature added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./styles.css";
 
 var username = "Rashita";
+var likeCount = 0;
 export default function App() {
   function onClickHandler() {
-    console.log("Clickedd!");
+    likeCount++;
+
+    console.log(likeCount);
   }
+
   return (
     <div className="App">
       <h1 style={{ color: "red" }}>Welcome</h1>
       <h2 style={{ color: "blue" }}>Hello, {username}! How are you?</h2>
       <button onClick={onClickHandler}>Like</button>
+      <h3>{likeCount}</h3>
     </div>
   );
 }
