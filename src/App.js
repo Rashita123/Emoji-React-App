26630c256e7c529e0b6f07f4453e777a8a770ce4
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-08 17:46:17 +0530

    onChange done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./styles.css";
 
 var username = "Rashita";
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
-  function onClickHandler() {
-    // likeCounter++;
-    setLikeCounter(likeCounter + 1);
-    console.log(likeCounter);
+  const [onchanger, setonchanger] = useState("");
+
+  function changeHandler(event) {
+    console.log(event.target.value);
+    setonchanger(event.target.value);
   }
 
   return (
     <div className="App">
       <h1 style={{ color: "red" }}>Welcome</h1>
       <h2 style={{ color: "blue" }}>Hello, {username}! How are you?</h2>
-      <button onClick={onClickHandler}>Like</button>
-      <h3>{likeCounter}</h3>
+      <input onChange={changeHandler}></input>
+      <div>Hello {onchanger}</div>
     </div>
   );
 }
