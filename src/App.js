b3eb4cc96cd7f8508a43565e79acd8e06b4d6ec6
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-08 14:55:16 +0530

    click feature added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles.css";
 
-var username=prompt("Tell me your name");
+var username = "Rashita";
 export default function App() {
+  function onClickHandler() {
+    console.log("Clickedd!");
+  }
   return (
     <div className="App">
-      <h1 style={{color: "red"}}>Welcome</h1>
-      <h2 style={{color: "blue"}}>Hello, {username}! How are you?</h2>
+      <h1 style={{ color: "red" }}>Welcome</h1>
+      <h2 style={{ color: "blue" }}>Hello, {username}! How are you?</h2>
+      <button onClick={onClickHandler}>Like</button>
     </div>
   );
 }
